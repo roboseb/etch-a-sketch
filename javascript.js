5df30bece65475fa541386b9fb47c0dfea0eafe7
roboseb <sebastienapril@rocketmail.com>
2022-03-27 20:30:50 -0600

    Add index.html,

---
+++ javascript.js
@@ -0,0 +1,11 @@
+console.log("cock time");
+
+const container = document.querySelector("#container");
+function makeCock() {
+    const div = document.createElement("div");
+    div.innerText = "COCK dfsdfsTEST";
+    container.appendChild(div);
+}
+const title = document.querySelector("h1");
+
+title.addEventListener()
